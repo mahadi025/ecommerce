41ae36bc3181977c44ce41a7899ba3d9b960b2fd
Mahadi karim Munif <mahadikarimmunif@gmail.com>
2022-02-11 18:29:16 +0600

    first commit

---
+++ static/js/cart.js
@@ -0,0 +1,65 @@
+const updateBtns=document.getElementsByClassName("update-cart")
+
+for( let i=0; i<updateBtns.length; i++ ){
+    updateBtns[i].addEventListener("click",function(){
+        let productId=this.dataset.product
+        let action=this.dataset.action
+        console.log("productId: ",productId,"Action: ",action)
+        
+        console.log("USER: ",user)
+        if(user==="AnonymousUser"){
+            addCookieItem(productId, action)
+        }
+        else{
+            updateUserOrder(productId,action)
+        }
+    })
+}
+
+function addCookieItem(productId, action){
+	console.log('User is not authenticated')
+
+	if (action == 'add'){
+		if (cart[productId] == undefined){
+		    cart[productId] = {'quantity':1}
+		}
+        else{
+			cart[productId]['quantity'] += 1
+		}
+	}
+
+	if (action == 'remove'){
+		cart[productId]['quantity'] -= 1
+		if (cart[productId]['quantity'] <= 0){
+			console.log('Item should be deleted')
+			delete cart[productId];
+		}
+	}
+	console.log('CART:', cart)
+	document.cookie ='cart=' + JSON.stringify(cart) + ";domain=;path=/"
+	window.location.reload(true);
+}
+
+function updateUserOrder(productId, action){
+    console.log("Sending Data")
+
+    let url="/update_item/"
+    fetch(url,{
+        method:"POST",
+        headers:{
+            "Content-Type": "application/json",
+            "X-CSRFToken":csrftoken,
+        },
+        body:JSON.stringify({"productId":productId,"action":action})
+    })
+    .then((response)=>{
+        return response.json()
+    })
+    .then((data)=>{
+        console.log("Data: ",data)
+        window.location.reload(true);
+        // setTimeout(function(){
+        //     parent.window.location.reload(true);
+        // },100); 
+    })
+}
---
+++ store/models.py
@@ -0,0 +1,87 @@
+import email
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Customer(models.Model):
+    user=models.OneToOneField(User,null=True,blank=True,on_delete=models.CASCADE)
+    name=models.CharField(max_length=200,null=True, blank=True)
+    email=models.EmailField(null=True, blank=True,max_length=254)
+    
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name=models.CharField(max_length=200,null=True, blank=True)
+    price=models.DecimalField(max_digits=7,decimal_places=2,null=True, blank=True)
+    digital=models.BooleanField(null=True, blank=True,default=False)
+    image = models.ImageField(null=True,blank=True)
+    description=models.TextField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
+    @property
+    def imageURL(self):
+        try:
+            url=self.image.url
+        except:
+            url=''
+        return url
+
+class Order(models.Model):
+    customer=models.ForeignKey(Customer,on_delete=models.SET_NULL,blank=True,null=True)
+    date_ordered=models.DateTimeField(auto_now_add=True)
+    complete=models.BooleanField(null=True, blank=True,default=False)
+    transaction_id=models.CharField(max_length=200,null=True, blank=True)
+    
+    def __str__(self):
+        return str(self.customer.name)+'('+str(self.date_ordered)+')'
+    
+    @property
+    def shipping(self):
+        shipping = False
+        orderitems = self.orderitem_set.all()
+        for i in orderitems:
+            if i.product.digital == False:
+                shipping = True
+        return shipping
+    
+    @property
+    def get_cart_total(self):
+        orderitems=self.orderitem_set.all()
+        total=sum([item.get_total for item in orderitems])
+        return total
+    
+    @property
+    def get_cart_items(self):
+        orderitems=self.orderitem_set.all()
+        total=sum([item.quantity for item in orderitems])
+        return total
+    
+class OrderItem(models.Model):
+    product=models.ForeignKey(Product,on_delete=models.SET_NULL,null=True, blank=True)
+    order=models.ForeignKey(Order,on_delete=models.SET_NULL,null=True, blank=True)
+    quantity=models.IntegerField(default=0,null=True, blank=True)
+    date_added=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return str(self.order.customer.name)+'('+str(self.product)+')'
+    
+    @property
+    def get_total(self):
+        total=self.product.price*self.quantity
+        return total
+    
+
+class ShippingAddress(models.Model):
+    customer=models.ForeignKey(Customer,on_delete=models.SET_NULL,blank=True,null=True)
+    order=models.ForeignKey(Order,on_delete=models.SET_NULL,null=True, blank=True)
+    address=models.CharField(max_length=300,null=False)
+    city=models.CharField(max_length=100,null=False)
+    state=models.CharField(max_length=100,null=False)
+    zipcode=models.CharField(max_length=10,null=False)
+    date_added=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.address
+    
